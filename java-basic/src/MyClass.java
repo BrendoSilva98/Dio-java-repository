7ab309e98d016a209b3d2ef7bbe3192af14ba52e
brendo98 <brendosilva@tutanota.com>
2024-06-14 20:25:54 -0300

    more basic stuff

--- java-basic/src/MyClass.java
+++ java-basic/src/MyClass.java
@@ -1,8 +1,23 @@
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.print("My first Class");
+
+        String firstName = "Brendo";
+        String secondName = "Silva da Costa";
+
+        String fullName = fullName (firstName, secondName);
+
+        System.out.println(fullName);
+
+    }
+    public static String fullName (String firstName, String secondName) {
+        return "Result of the method " + firstName.concat(" ").concat(secondName);
         
     }
     
 }
+
+        /*System.out.print("My first Class");
+        String myName = "Brendo";
+        int anoFabricacao = 2022;
+        boolean isItTrue = true;*/
